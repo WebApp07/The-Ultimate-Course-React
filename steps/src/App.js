fbc8213a34b7d5a71b1abe250172ba0b68b6e02d
WebApp07 <aminezouguig@proton.me>
2025-02-10 12:14:47 -0800

    Updating State Based on Current State

--- steps/src/App.js
+++ steps/src/App.js
@@ -15,17 +15,17 @@ function App() {
 
   function handlerPrevious() {
     //alert("Previous!");
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handlerNext() {
     //alert("Next!");
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
 

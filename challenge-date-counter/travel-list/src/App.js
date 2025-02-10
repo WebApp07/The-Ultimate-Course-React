6e546efa24d4ca133e182ace63e59feb1384ea60
WebApp07 <aminezouguig@proton.me>
2025-02-10 15:01:33 -0800

    Setyp project, Building the Layout

---
+++ challenge-date-counter/travel-list/src/App.js
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🎒</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🎒 You have X item on your list, ad you already packed X (X%)</em>
+    </footer>
+  );
+}
